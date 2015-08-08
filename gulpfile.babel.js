29d931c4c83fa39c385f9721738a65a4c07ef5c2
Jesse B. Hannah <jesse@jbhannah.net>
2015-08-08 14:57:08 -0700

    renderContent and prepare for Nunjucks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,10 @@
-import gulp from 'gulp';
+import assign from 'lodash/object/assign';
 import frontMatter from 'front-matter';
+import gulp from 'gulp';
+import nunjucks from 'nunjucks';
+import through from 'through2';
+
+const DEST = 'build';
 
 let siteData = {
   title: 'Jesse B. Hannah',
@@ -9,4 +14,43 @@ let siteData = {
   buildTime: new Date()
 };
 
-gulp.task('default', function () {});
+let env = nunjucks.configure('templates', {
+  autoescape: false,
+  watch: false
+});
+
+function renderContent () {
+  let files = [];
+  let site = assign(siteData);
+
+  return through.obj(
+      function transform(file, enc, done) {
+        let contents = file.contents.toString();
+        let yaml = frontMatter(contents);
+
+        if (yaml.attributes) {
+          contents = yaml.body;
+          file.data = {
+            site: site,
+            page: assign(yaml.attributes)
+          };
+        }
+
+        file.contents = new Buffer(contents);
+
+        files.push(file);
+        done();
+      },
+      function flush(done) {
+        files.forEach(function (file) { this.push(file); }.bind(this));
+        done();
+      }
+  );
+}
+
+gulp.task('pages', function () {
+  gulp.src(['./pages/*'], {base: '.'})
+    .pipe(renderContent());
+});
+
+gulp.task('default', ['pages']);
