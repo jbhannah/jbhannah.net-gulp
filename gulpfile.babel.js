c6c2052796b203676ddc31bed3ea75ce5d7419c7
Jesse B. Hannah <jesse@jbhannah.net>
2015-08-08 14:16:21 -0700

    Site data

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,12 @@
 import gulp from 'gulp';
 import frontMatter from 'front-matter';
 
+let siteData = {
+  title: 'Jesse B. Hannah',
+  subtitle: 'jbhannah',
+  baseUrl: 'https://jbhannah.net',
+  timezone: 'America/Phoenix',
+  buildTime: new Date()
+};
+
 gulp.task('default', function () {});
