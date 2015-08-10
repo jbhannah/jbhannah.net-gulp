fec55530740df8278cf77da3bda2d634fb5be18f
Jesse B. Hannah <jesse@jbhannah.net>
2015-08-09 17:22:45 -0700

    Cleanup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -25,7 +25,6 @@ function getPermalink(filepath) {
   let extname = path.extname(filepath);
   let dirname = path.dirname(filepath);
   let basename = path.basename(filepath, extname);
-  console.log(basename);
 
   if (basename !== 'index') {
     dirname += '/' + basename;
@@ -49,44 +48,44 @@ function renderContent() {
   let site = assign(siteData);
 
   return through.obj(
-      function transform(file, enc, done) {
-        let contents = file.contents.toString();
-        let yaml = frontMatter(contents);
+    function transform(file, enc, done) {
+      let contents = file.contents.toString();
+      let yaml = frontMatter(contents);
 
-        if (yaml.attributes) {
-          contents = yaml.body;
-          file.data = {
-            site: site,
-            page: assign({permalink: getPermalink(file.path)}, yaml.attributes)
-          };
-        }
-
-        file.contents = new Buffer(contents);
-
-        files.push(file);
-        done();
-      },
-      function flush(done) {
-        files.forEach(function (file) { this.push(file); }.bind(this));
-        done();
+      if (yaml.attributes) {
+        contents = yaml.body;
+        file.data = {
+          site: site,
+          page: assign({permalink: getPermalink(file.path)}, yaml.attributes)
+        };
       }
+
+      file.contents = new Buffer(contents);
+
+      files.push(file);
+      done();
+    },
+    function flush(done) {
+      files.forEach(function (file) { this.push(file); }.bind(this));
+      done();
+    }
   );
 }
 
 function renderTemplate() {
   return through.obj(
-      function (file, enc, cb) {
-        let content = file.contents.toString();
-        file.data.page.content = nunjucks.renderString(content, file.data);
+    function (file, enc, cb) {
+      let content = file.contents.toString();
+      file.data.page.content = nunjucks.renderString(content, file.data);
 
-        let template = file.data.page.template;
-        file.contents = new Buffer(nunjucks.render(template, file.data));
+      let template = file.data.page.template;
+      file.contents = new Buffer(nunjucks.render(template, file.data));
 
-        file.path = getDestPathFromPermalink(file.data.page.permalink);
+      file.path = getDestPathFromPermalink(file.data.page.permalink);
 
-        this.push(file);
-        cb();
-      }
+      this.push(file);
+      cb();
+    }
   );
 }
 
