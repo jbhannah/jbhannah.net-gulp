f1c1af887ae218ec1be5a51f07ee5a5fcfd983b7
Jesse B. Hannah <jesse@jbhannah.net>
2016-03-29 12:22:22 -0700

    Convert displayed dates to local timezone
    Fixes #157

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
-console.log('Hello, world!');
+import moment from 'moment';
+
+let times = document.getElementsByTagName('time');
+
+for (let i = 0; i < times.length; i++) {
+  times[i].textContent = moment(times[i].attributes.datetime.value).format('D MMMM YYYY');
+}
